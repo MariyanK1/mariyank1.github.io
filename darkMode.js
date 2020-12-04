ec33a0ae39b2b42ac9d29ebc6882aa9d700c24a0
Mariyan Maksimov <mariyan.pickup@gmail.com>
2020-12-04 17:03:48 +0100

    Update darkMode.js

--- darkMode.js
+++ darkMode.js
@@ -5,7 +5,8 @@ function darkMode() {
         h1: () => document.getElementsByTagName('h1')[0],
         p: () => document.getElementsByTagName('p')[0],
         h2: () => document.getElementsByTagName('h2')[0],
-        span: () => document.querySelector('span')
+        span: () => document.querySelector('span'),
+        img: () => document.querySelector('#pic')
 
     }
 
@@ -19,6 +20,7 @@ function darkMode() {
             $selectors.p().style.color = 'white';
             $selectors.h2().style.color = 'white';
             $selectors.span().style.color = 'white';
+            $selectors.img().src = "https://images2.imgbox.com/b1/8f/nYGqGA6p_o.png";
 
         } else {
             document.body.style.backgroundColor = 'white';
@@ -27,6 +29,7 @@ function darkMode() {
             $selectors.p().style.color = 'black';
             $selectors.h2().style.color = 'black';
             $selectors.span().style.color = 'black';
+            $selectors.img().src = "https://avatars2.githubusercontent.com/u/58628678?s=400&u=924861bafd3fddf4103190598da3b9f59f495983&v=4";
         }
     })
 }
