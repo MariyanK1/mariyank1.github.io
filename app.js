1c96c38db48dbd206867cb5fbeb33588173eb274
Mariyan Maksimov <mariyan.pickup@gmail.com>
2020-12-04 14:46:45 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,59 +1,32 @@
-const urlCountries = 'https://restcountries.eu/rest/v2/all';
-const urlQuotes = 'https://api.kanye.rest';
+function darkMode() {
 
-const $domeElements = {
-    input: () => document.querySelector('#towns'),
-    loadBtn: () => document.querySelector('#btnLoadTowns'),
-    root: () => document.querySelector('#root'),
-    load250: () => document.querySelector('#btnLoad250'),
-    loadKanye: () => document.querySelector('#btnLoadQuote'),
-    ul: () => document.querySelector('#root > ul')
+    const $selectors = {
+        btn: () => document.querySelector('#darkMode'),
+        h1: () => document.getElementsByTagName('h1'),
+        p: () => document.getElementsByTagName('p'),
+        h2: () => document.getElementsByTagName('h2'),
+        span: () => document.querySelector('span')
 
-}
+    }
 
-$domeElements.loadBtn().addEventListener('click', loadTowns);
-$domeElements.load250().addEventListener('click', load250);
-$domeElements.loadKanye().addEventListener('click', loadWest);
+    $selectors.btn().addEventListener('click', e => {
+        e.preventDefault();
 
-function loadWest(e) {
-    e.preventDefault();
+        if ($selectors.btn().textContent === 'switch to dark mode') {
+            $selectors.btn().textContent = 'switch to light mode';
+            document.body.style.backgroundColor = 'black';
+            $selectors.h1()[0].style.color = 'white';
+            $selectors.p()[0].style.color = 'white';
+            $selectors.h2()[0].style.color = 'white';
+            $selectors.span().style.color = 'white';
 
-    fetch(urlQuotes)
-        .then(r => r.json())
-        .then(({ quote }) => {
-            $domeElements.ul().innerHTML = `<lu><li>${quote}</li></lu>`;
-        })
-        .catch(e => console.error(e));
-}
-
-function load250(e) {
-    e.preventDefault();
-    fetch(urlCountries)
-        .then(r => r.json())
-        .then(appendCities)
-        .catch(e => aconsole.error(e));
-}
-
-function loadTowns(e) {
-    e.preventDefault();
-
-    const { value } = $domeElements.input();
-    const towns = value.split(', ').map((t) => { return { name: t } });
-
-    appendCities(towns);
-}
-
-function appendCities(towns) {
-    getTemplate()
-        .then(tempSource => {
-            const template = Handlebars.compile(tempSource);
-
-            const html = template({ towns });
-
-            $domeElements.root().innerHTML = html;
-        });
-}
-
-function getTemplate() {
-    return fetch('./template.hbs').then((r) => r.text());
+        } else {
+            $selectors.btn().textContent = 'switch to dark mode';
+            $selectors.h1()[0].style.color = 'black';
+            $selectors.p()[0].style.color = 'black';
+            $selectors.h2()[0].style.color = 'black';
+            document.body.style.backgroundColor = 'white';
+            $selectors.span().style.color = 'black';
+        }
+    })
 }
