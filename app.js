8e3965eb7040d5d6f508fb74845cf36ebac5bde5
Mariyan Maksimov <mariyan.pickup@gmail.com>
2020-12-04 14:58:51 +0100

    refactor #1

--- app.js
+++ app.js
@@ -2,9 +2,9 @@ function darkMode() {
 
     const $selectors = {
         btn: () => document.querySelector('#darkMode'),
-        h1: () => document.getElementsByTagName('h1'),
-        p: () => document.getElementsByTagName('p'),
-        h2: () => document.getElementsByTagName('h2'),
+        h1: () => document.getElementsByTagName('h1')[0],
+        p: () => document.getElementsByTagName('p')[0],
+        h2: () => document.getElementsByTagName('h2')[0],
         span: () => document.querySelector('span')
 
     }
@@ -13,19 +13,19 @@ function darkMode() {
         e.preventDefault();
 
         if ($selectors.btn().textContent === 'switch to dark mode') {
-            $selectors.btn().textContent = 'switch to light mode';
             document.body.style.backgroundColor = 'black';
-            $selectors.h1()[0].style.color = 'white';
-            $selectors.p()[0].style.color = 'white';
-            $selectors.h2()[0].style.color = 'white';
+            $selectors.btn().textContent = 'switch to light mode';
+            $selectors.h1().style.color = 'white';
+            $selectors.p().style.color = 'white';
+            $selectors.h2().style.color = 'white';
             $selectors.span().style.color = 'white';
 
         } else {
-            $selectors.btn().textContent = 'switch to dark mode';
-            $selectors.h1()[0].style.color = 'black';
-            $selectors.p()[0].style.color = 'black';
-            $selectors.h2()[0].style.color = 'black';
             document.body.style.backgroundColor = 'white';
+            $selectors.btn().textContent = 'switch to dark mode';
+            $selectors.h1().style.color = 'black';
+            $selectors.p().style.color = 'black';
+            $selectors.h2().style.color = 'black';
             $selectors.span().style.color = 'black';
         }
     })
