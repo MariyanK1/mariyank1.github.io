ddcf3c35c92ada5f2c43b40771c0cb0766e6054a
Mariyan Maksimov <mariyan.pickup@gmail.com>
2020-12-05 18:26:12 +0100

    Update darkMode.js

--- darkMode.js
+++ darkMode.js
@@ -6,30 +6,60 @@ function darkMode() {
         p: () => document.getElementsByTagName('p')[0],
         h2: () => document.getElementsByTagName('h2')[0],
         span: () => document.querySelector('span'),
-        img: () => document.querySelector('#pic')
+        img: () => document.querySelector('#pic'),
+        thatsAll: () => document.querySelector('#thatsAll'),
+        formValidator: () => document.querySelector('#formValidator'),
+        techUSed: () => document.querySelector('#techUsed'),
 
     }
 
     $selectors.btn().addEventListener('click', e => {
         e.preventDefault();
+        switch ($selectors.btn().textContent) {
+            case 'switch to dark mode':
+                document.body.style.backgroundColor = 'black';
+                $selectors.btn().textContent = 'switch to light mode';
+                $selectors.img().src = "https://images2.imgbox.com/b1/8f/nYGqGA6p_o.png";
+                $selectors.thatsAll().src = "https://images2.imgbox.com/1c/c1/bLYVqJtQ_o.png";
+                changeColorWhite($selectors.p());
+                changeColorWhite($selectors.h1());
+                changeColorWhite($selectors.h2());
+                changeColorWhite($selectors.span());
+                changeColorWhite($selectors.formValidator());
+                changeColorWhite($selectors.techUSed());
+                break;
 
-        if ($selectors.btn().textContent === 'switch to dark mode') {
-            document.body.style.backgroundColor = 'black';
-            $selectors.btn().textContent = 'switch to light mode';
-            $selectors.h1().style.color = 'white';
-            $selectors.p().style.color = 'white';
-            $selectors.h2().style.color = 'white';
-            $selectors.span().style.color = 'white';
-            $selectors.img().src = "https://images2.imgbox.com/b1/8f/nYGqGA6p_o.png";
-
-        } else {
-            document.body.style.backgroundColor = 'white';
-            $selectors.btn().textContent = 'switch to dark mode';
-            $selectors.h1().style.color = 'black';
-            $selectors.p().style.color = 'black';
-            $selectors.h2().style.color = 'black';
-            $selectors.span().style.color = 'black';
-            $selectors.img().src = "https://avatars2.githubusercontent.com/u/58628678?s=400&u=924861bafd3fddf4103190598da3b9f59f495983&v=4";
+            default:
+                document.body.style.backgroundColor = 'white';
+                $selectors.btn().textContent = 'switch to dark mode';
+                $selectors.img().src = "https://avatars2.githubusercontent.com/u/58628678?s=400&u=924861bafd3fddf4103190598da3b9f59f495983&v=4";
+                $selectors.thatsAll().src = "https://images2.imgbox.com/66/b2/3NDjZFfy_o.png";
+                changeColorBlack($selectors.h1());
+                changeColorBlack($selectors.p());
+                changeColorBlack($selectors.h2());
+                changeColorBlack($selectors.span());
+                changeColorBlack($selectors.formValidator());
+                changeColorBlack($selectors.techUSed());
+                break;
         }
+
     })
+
+
+    function changeColorBlack(el) {
+        if (el) {
+            el.style.color = 'black';
+            return el;
+        }
+
+    }
+
+    function changeColorWhite(el) {
+        if (el) {
+            el.style.color = 'white';
+            return el;
+        }
+
+    }
+
 }
