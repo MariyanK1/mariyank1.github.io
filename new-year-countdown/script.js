4646cffafaecefa2f0889e1b19fb030bf05ebe1a
Mariyan Maksimov <mariyan.pickup@gmail.com>
2020-12-24 14:55:30 +0100

    Update script.js

--- new-year-countdown/script.js
+++ new-year-countdown/script.js
@@ -28,11 +28,11 @@ function generateWish(e) {
 
     const wishes = [
         'New Year’s is the time to forget all your fears, drink a few beers, leave behind all your tears!',
-        'New years are like restart buttons. You think you can push the button and start things all over again but then realize your life is too messed up to be restarted fresh!',
-        'I hope you start the New Year as the startup of your old habits. Have the same old beautiful life in a box of New Year!',
-        'I would quit all my bad habits for the new year, but then I remembered that nobody likes a quitter.',
-        'May this upcoming year actually bring change in you – not just all your old habits wrapped in a new package, Oh God! Happy New year, anyways.',
-        'I wish even your success amount comes on my account and the right hands can spend them this upcoming year. Happy New Year.'
+        'Happy New Year to you and your family! Wishing you 365 days of good luck!',
+        'Happy new year to the sweetest person alive. Thanks for being in my life.',
+        'Happy new year to the best mother in the world! Wish you a blessed life.',
+        'I wish you a very Happy New Year. Hope you have a great time ahead.',
+        'Happy New Year! I hope all your endeavors in 2021 are successful.'
     ]
 
     $domElements.divWishes().innerHTML = wishes[Math.floor(Math.random() * Math.floor(6))];
