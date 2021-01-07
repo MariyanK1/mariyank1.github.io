d5809e7dc874c1e4d36b1c980ae530777c842e29
Mariyan Maksimov <mariyan.pickup@gmail.com>
2021-01-07 07:30:02 +0100

    Update MarketCap.js

--- crypto-market/crypto-currency-project/components/MarketCap.js
+++ crypto-market/crypto-currency-project/components/MarketCap.js
@@ -2,12 +2,9 @@ import React from "react";
 
 function MarketCap(props) {
   return (
-    <>
-      {" "}
       <td>
         <strong>${props.value.toLocaleString()}</strong>
       </td>
-    </>
   );
 }
 
