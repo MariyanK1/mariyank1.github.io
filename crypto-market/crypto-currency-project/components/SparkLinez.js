6fd4a92185698450ad229e1ba1ccaeb2824a797d
Mariyan Maksimov <mariyan.pickup@gmail.com>
2021-01-07 07:30:49 +0100

    Update SparkLinez.js

--- crypto-market/crypto-currency-project/components/SparkLinez.js
+++ crypto-market/crypto-currency-project/components/SparkLinez.js
@@ -3,14 +3,12 @@ import React from "react";
 
 function SparkLinez(props) {
   return (
-    <>
       <td>
         <Sparklines data={props.spark} limit={100} width={100} height={60}>
           <SparklinesLine color="green" />
           <SparklinesSpots />
         </Sparklines>
       </td>
-    </>
   );
 }
 
