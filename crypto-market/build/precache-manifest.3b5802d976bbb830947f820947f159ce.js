c9044834feac6b6875850dc3fc2094bc0af1242f
MariyanK1 <mariyan.pickup@gmail.com>
2021-03-11 17:22:58 +0200

    build

---
+++ crypto-market/build/precache-manifest.3b5802d976bbb830947f820947f159ce.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e38f784fe34b19cac2b35e39cc8a337a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "29d9eb7eafdc8b70da89",
+    "url": "/static/css/main.b6e9b597.chunk.css"
+  },
+  {
+    "revision": "d628c4b526be1140caa9",
+    "url": "/static/js/2.b85b2dc3.chunk.js"
+  },
+  {
+    "revision": "569e878394263cd68864b6526c81f10b",
+    "url": "/static/js/2.b85b2dc3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "29d9eb7eafdc8b70da89",
+    "url": "/static/js/main.b6985aa0.chunk.js"
+  },
+  {
+    "revision": "8740fb2f5d96f13b215f",
+    "url": "/static/js/runtime-main.18e1c4e7.js"
+  }
+]);
