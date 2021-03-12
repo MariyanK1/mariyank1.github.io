8127c06bfa334286dce20f6746f1f3289261abd2
MariyanK1 <mariyan.pickup@gmail.com>
2021-03-12 16:30:40 +0200

    converted User Component to func component

--- github-finder/src/components/users/User.js
+++ github-finder/src/components/users/User.js
@@ -1,94 +1,92 @@
-import React, { Component } from "react";
+import { useEffect } from "react";
 import { Link } from "react-router-dom";
 
 import Repos from "../repos/Repos";
 
-class User extends Component {
-  componentDidMount() {
-    this.props.getUser(this.props.match.params.login);
-    this.props.getUserRepos(this.props.match.params.login);
-  }
+function User({ user, repos, loading, getUser, getUserRepos, match }) {
+  useEffect(() => {
+    getUser(match.params.login);
+    getUserRepos(match.params.login);
+    // eslint-disable-next-line
+  }, []);
 
-  render() {
-    const {
-      name,
-      avatar_url,
-      location,
-      bio,
-      blog,
-      login,
-      html_url,
-      followers,
-      following,
-      public_repos,
-      public_gists,
-      hireable,
-    } = this.props.user;
+  const {
+    name,
+    avatar_url,
+    location,
+    bio,
+    blog,
+    login,
+    html_url,
+    followers,
+    following,
+    public_repos,
+    public_gists,
+    hireable,
+  } = user;
 
-    const { loading, repos } = this.props;
-    if (loading) return <h1>Loading</h1>;
-    return (
-      <>
-        <Link to="/" className="btn btn-light">
-          Back To Search
-        </Link>
-        Hireable:
-        {hireable ? (
-          <i className="fas fa-check text-success" />
-        ) : (
-          <i className="fas fa-times-circle text-danger" />
-        )}
-        <div className="card grid-2">
-          <div className="all-center">
-            <img
-              src={avatar_url}
-              className="round-img"
-              style={{ width: "150px" }}
-              alt="user-avatar"
-            />
-            <h1>{name}</h1>
-            Location: {location}
-          </div>
-          <div>
-            {bio && (
-              <>
-                <h3>Bio:</h3> <p>{bio}</p>
-              </>
-            )}
-            <a href={html_url} className="btn btn-dark my-1">
-              Visit Github profile
-            </a>
-            <ul>
-              <li>
-                {login && (
-                  <>
-                    <strong>Username:</strong> {login}
-                  </>
-                )}
-              </li>
-              <li>
-                {blog && (
-                  <>
-                    <strong>Website: </strong>
-                    <a href={blog} rel="noreferrer" target="_blank">
-                      {blog}
-                    </a>
-                  </>
-                )}
-              </li>
-            </ul>
-          </div>
+  if (loading) return <h1>Loading</h1>;
+  return (
+    <>
+      <Link to="/" className="btn btn-light">
+        Back To Search
+      </Link>
+      Hireable:
+      {hireable ? (
+        <i className="fas fa-check text-success" />
+      ) : (
+        <i className="fas fa-times-circle text-danger" />
+      )}
+      <div className="card grid-2">
+        <div className="all-center">
+          <img
+            src={avatar_url}
+            className="round-img"
+            style={{ width: "150px" }}
+            alt="user-avatar"
+          />
+          <h1>{name}</h1>
+          Location: {location}
         </div>
-        <div className="card text-center">
-          <div className="badge badge-primary">Followers: {followers}</div>
-          <div className="badge badge-success">Following: {following}</div>
-          <div className="badge badge-light">Public Repos: {public_repos}</div>
-          <div className="badge badge-dark">Public Gists: {public_gists}</div>
+        <div>
+          {bio && (
+            <>
+              <h3>Bio:</h3> <p>{bio}</p>
+            </>
+          )}
+          <a href={html_url} className="btn btn-dark my-1">
+            Visit Github profile
+          </a>
+          <ul>
+            <li>
+              {login && (
+                <>
+                  <strong>Username:</strong> {login}
+                </>
+              )}
+            </li>
+            <li>
+              {blog && (
+                <>
+                  <strong>Website: </strong>
+                  <a href={blog} rel="noreferrer" target="_blank">
+                    {blog}
+                  </a>
+                </>
+              )}
+            </li>
+          </ul>
         </div>
-        <Repos repos={repos} />
-      </>
-    );
-  }
+      </div>
+      <div className="card text-center">
+        <div className="badge badge-primary">Followers: {followers}</div>
+        <div className="badge badge-success">Following: {following}</div>
+        <div className="badge badge-light">Public Repos: {public_repos}</div>
+        <div className="badge badge-dark">Public Gists: {public_gists}</div>
+      </div>
+      <Repos repos={repos} />
+    </>
+  );
 }
 
 export default User;
