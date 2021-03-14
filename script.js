c6929404ed9a024c1c815bd4d14c98d6923315c5
MariyanK1 <mariyan.pickup@gmail.com>
2021-03-14 23:56:23 +0200

    small refactor

--- script.js
+++ script.js
@@ -3,6 +3,11 @@ function darkMode() {
     btn: () => document.querySelector(".darkModeButton"),
     h1: () => document.getElementsByTagName("h1")[0],
     p: () => document.getElementsByTagName("p")[0],
+    p2: () => document.getElementsByTagName("p")[1],
+    p3: () => document.getElementsByTagName("p")[2],
+    p4: () => document.getElementsByTagName("p")[3],
+    p5: () => document.getElementsByTagName("p")[4],
+    p6: () => document.querySelector(".cert"),
     h2: () => document.getElementsByTagName("h2")[0],
     span: () => document.querySelector("span"),
     meal_finder: () => document.querySelectorAll(".projectTitle"),
@@ -19,6 +24,11 @@ function darkMode() {
         $selectors.btn().textContent = "switch to light mode";
         changeColorWhite($selectors.contact());
         changeColorWhite($selectors.p());
+        changeColorWhite($selectors.p2());
+        changeColorWhite($selectors.p3());
+        changeColorWhite($selectors.p4());
+        changeColorWhite($selectors.p5());
+        changeColorWhite($selectors.p6());
         changeColorWhite($selectors.h1());
         changeColorWhite($selectors.h2());
         changeColorWhite($selectors.span());
@@ -34,6 +44,11 @@ function darkMode() {
         changeColorBlack($selectors.contact());
         changeColorBlack($selectors.h1());
         changeColorBlack($selectors.p());
+        changeColorBlack($selectors.p2());
+        changeColorBlack($selectors.p3());
+        changeColorBlack($selectors.p4());
+        changeColorBlack($selectors.p5());
+        changeColorBlack($selectors.p6());
         changeColorBlack($selectors.h2());
         changeColorBlack($selectors.span());
         changeColorBlack($selectors.meal_finder());
