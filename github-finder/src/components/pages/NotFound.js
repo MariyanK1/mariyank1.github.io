fea6254a7bf60e1a83f8c9d5f5402d8e426e9fce
Mariyan Maksimov <mariyan.pickup@gmail.com>
2021-03-15 14:00:40 +0200

    Delete github-finder directory

--- github-finder/src/components/pages/NotFound.js
+++
@@ -1,9 +0,0 @@
-function NotFound() {
-  return (
-    <>
-      <h1>Not Found</h1>
-      <p className="lead">The page you are looking for does not exist!</p>
-    </>
-  );
-}
-export default NotFound;
